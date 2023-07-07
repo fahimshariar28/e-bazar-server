7bd66c1428571f156419deb13a0dd3a1382e777b
Fahim Shariar <fahimshariar28@gmail.com>
2023-07-07 15:42:15 +0600

    Method change

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ async function run() {
     });
 
     // Update order status
-    app.patch("/updateorder/:id", async (req, res) => {
+    app.put("/updateorder/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const update = { $set: { status: "delivered" } };
