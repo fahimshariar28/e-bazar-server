0801c9028078b037f3352005e53d63afa989e521
Fahim Shariar <fahimshariar28@gmail.com>
2023-07-07 15:55:09 +0600

    Customer delete

--- index.js
+++ index.js
@@ -201,6 +201,14 @@ async function run() {
       res.send(result);
     });
 
+    // Delete customer
+    app.delete("/deletecustomer/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
